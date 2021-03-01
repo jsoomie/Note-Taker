4dad895a1bb335921cbfb20aef73b9c522735bf8
Jonathan Soukaseume <sksmejn@gmail.com>
2021-03-01 08:33:56 -0800

    added boiler plate express index.js

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("Hello World");
+// Pulls in modules
+const express = require("express");
+const fs = require("fs");
+
+// Init app
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Start server
+app.listen(PORT, () => console.log(`Server listening on PORT: ${PORT}`));
