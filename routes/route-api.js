067dd8f24b2bec65d5fe23233cc011d4829832c4
Jonathan Soukaseume <sksmejn@gmail.com>
2021-03-03 11:40:37 -0800

    removed some console.logs for testing

--- routes/route-api.js
+++ routes/route-api.js
@@ -4,31 +4,35 @@ const uuid = require("uuid"); // Pulls in ID to create unique ids
 const fs = require("fs"); // manipulate file systems
 const path = require("path"); // Pulls in path
 
+// Gets any notes to load beforehand
 router.get("/notes", (req, res) => {
     const dbPath = "../db/db.json";
     res.sendFile(path.join(__dirname, dbPath));
-    console.log("This is the api router.get");
 })
 
+// Able to post notes 
 router.post('/notes', (req, res) => {
-    console.log("This is api router.post ");
-
-    const note = JSON.parse(fs.readFileSync("./db/db.json"));
+    const dbPath = "./db/db.json";
+    const note = JSON.parse(fs.readFileSync(dbPath));
     const newNote = req.body;
+    // Creates ID for notes
     newNote.id = uuid.v4();
+    // Pushes the new note into the db.json
     note.push(newNote);
-    fs.writeFileSync("./db/db.json", JSON.stringify(note))
+    fs.writeFileSync(dbPath, JSON.stringify(note))
     res.json(note);
 })
 
+// Deletes notes according to id
 router.delete("/notes/:id", (req, res) => {
-    console.log("this is api router.delete");
-
-    const note = JSON.parse(fs.readFileSync("./db/db.json"));
+    const dbPath = "./db/db.json";
+    const note = JSON.parse(fs.readFileSync(dbPath));
+    // Delete notes according to their id with the filter method
     const deleteNote = note.filter((delNote) => delNote.id !== req.params.id);
-    fs.writeFileSync("./db/db.json", JSON.stringify(deleteNote));
+    fs.writeFileSync(dbPath, JSON.stringify(deleteNote));
     res.json(deleteNote);
 })
 
+// Exports
 module.exports = router;
 
