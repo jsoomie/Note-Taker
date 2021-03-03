931a15f5b6c9510ad29b40cae1e71cefd36404d4
Jonathan Soukaseume <sksmejn@gmail.com>
2021-03-02 17:36:53 -0800

    added required packages and created a port that works

--- index.js
+++ index.js
@@ -2,9 +2,25 @@
 const express = require("express");
 const fs = require("fs");
 
-// Init app
+const db = require("./db/db.json");
+
+// Init app and server
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+// Body Parser middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/", (req, res) => {
+    res.send("Hello from express!");
+    console.log(db);
+})
+
+app.post("/", (req, res) => {
+    console.log("Post request to the home page");
+})
+
 
 // Start server
 app.listen(PORT, () => console.log(`Server listening on PORT: ${PORT}`));
