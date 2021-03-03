da854667ece8cfd8c4ebc9abe30491b81e898859
Jonathan Soukaseume <sksmejn@gmail.com>
2021-03-02 19:01:36 -0800

    added routes to homepage and notes page

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 // Pulls in modules
 const express = require("express");
 const fs = require("fs");
+const path = require("path");
 
+// Pulls in the routes
+const routeHtml = require("./routes/route-html.js");
+
+// Pulls in database
 const db = require("./db/db.json");
 
 // Init app and server
@@ -12,15 +17,20 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.get("/", (req, res) => {
-    res.send("Hello from express!");
-    console.log(db);
-})
 
+
+// Directs to homepage
+// app.get("/", (req, res) => {
+//     res.sendFile(path.join(__dirname, homepage));
+//     console.log(db);
+// })
+
+app.use("/", routeHtml);
+
+// Get posts
 app.post("/", (req, res) => {
     console.log("Post request to the home page");
 })
-
 
 // Start server
 app.listen(PORT, () => console.log(`Server listening on PORT: ${PORT}`));
---
+++ routes/route-html.js
@@ -0,0 +1,21 @@
+// Pulls in required modules
+const path = require("path");
+const express = require("express");
+const router = express.Router();
+
+// Creates homepage link
+const homepage = "../public/index.html";
+const notepage = "../public/notes.html";
+
+// Routes to homepage
+router.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, homepage));
+});
+
+// Routes to notes page
+router.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, notepage));
+})
+
+// Exports
+module.exports = router;
